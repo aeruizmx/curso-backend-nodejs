fbde22c9dcf8319bb239c93145d2e9d20c5cc888
Andres Ruiz <aruiz@grupooctano.com.mx>
2021-03-16 10:37:27 -0600

    Reto

--- movies-api/index.js
+++ movies-api/index.js
@@ -9,6 +9,15 @@ app.get('/', function(req, res) {
 app.get('/json', function(req, res) {
   res.json({'hello':  'world'})
 })
+app.get('/:year', function(req, res) {
+  const {year} = req.params;
+  if(year % 4 == 0 ){
+    res.send('Año bisiesto!')
+  } else {
+    res.send('Año no bisiesto!')
+  }
+
+})
 app.listen(config.port, function(){
   console.log(`Listening http://localhost:${config.port}`);
 })

0f7ead2f27be70fc92929f5104104d19360e21c7
Andres Ruiz <aruiz@grupooctano.com.mx>
2021-03-16 10:29:05 -0600

    Creando tu primer servidor con Express.js

---
+++ movies-api/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const { config } = require('./config/index');
+
+app.get('/', function(req, res) {
+  res.send('hello world hello')
+})
+
+app.get('/json', function(req, res) {
+  res.json({'hello':  'world'})
+})
+app.listen(config.port, function(){
+  console.log(`Listening http://localhost:${config.port}`);
+})

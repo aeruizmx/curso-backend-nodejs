204b259de707e5c77b208da26f110cb0e8929709
Andres Ruiz <aruiz@grupooctano.com.mx>
2021-03-16 18:49:09 -0600

    Implementando un CRUD en Express.js

--- movies-api/index.js
+++ movies-api/index.js
@@ -1,23 +1,24 @@
 const express = require('express');
 const app = express();
 const { config } = require('./config/index');
+const moviesApi = require('./routes/movies');
+moviesApi(app);
+// app.get('/', function(req, res) {
+//   res.send('hello world hello')
+// })
 
-app.get('/', function(req, res) {
-  res.send('hello world hello')
-})
+// app.get('/json', function(req, res) {
+//   res.json({'hello':  'world'})
+// })
+// app.get('/:year', function(req, res) {
+//   const {year} = req.params;
+//   if(year % 4 == 0 ){
+//     res.send('Año bisiesto!')
+//   } else {
+//     res.send('Año no bisiesto!')
+//   }
 
-app.get('/json', function(req, res) {
-  res.json({'hello':  'world'})
-})
-app.get('/:year', function(req, res) {
-  const {year} = req.params;
-  if(year % 4 == 0 ){
-    res.send('Año bisiesto!')
-  } else {
-    res.send('Año no bisiesto!')
-  }
-
-})
+//})
 app.listen(config.port, function(){
   console.log(`Listening http://localhost:${config.port}`);
 })

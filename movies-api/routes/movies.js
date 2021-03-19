e4c118a26d1e467119c212b707067a2806308f95
Andres Ruiz <aruiz@grupooctano.com.mx>
2021-03-18 19:50:29 -0600

    ¿Cómo implementar una capa de manejo de caché?

--- movies-api/routes/movies.js
+++ movies-api/routes/movies.js
@@ -2,14 +2,16 @@ const express = require('express');
 const MoviesService = require('../services/movies');
 const { movieIdSchema, createMovieSchema, updateMovieSchema } = require('../utils/schemas/movies');
 const validationHandler = require('../utils/middleware/validationHandler');
-
-function moviesApi(app){
+const cacheResponse = require('../utils/cacheResponse');
+const { FIVE_MINUTES_IN_SECONDS, SIXTY_MINUTES_IN_SECONDS } = require('../utils/time');
+function moviesApi(app) {
   const router = express.Router();
   app.use('/api/movies', router);
 
   const movieServices = new MoviesService();
 
-  router.get('/', async function(req, res, next){
+  router.get('/', async function (req, res, next) {
+    cacheResponse(res, FIVE_MINUTES_IN_SECONDS);
     const { tags } = req.query;
     try {
       const movies = await movieServices.getMovies({ tags });
@@ -23,7 +25,8 @@ function moviesApi(app){
     }
   });
 
-  router.get('/:movieId', validationHandler({ movieId: movieIdSchema }, 'params'), async function(req, res, next){
+  router.get('/:movieId', validationHandler({ movieId: movieIdSchema }, 'params'), async function (req, res, next) {
+    cacheResponse(res, SIXTY_MINUTES_IN_SECONDS);
     const { movieId } = req.params;
     try {
       const movies = await movieServices.getMovie({ movieId });
@@ -36,7 +39,7 @@ function moviesApi(app){
     }
   });
 
-  router.post('/', validationHandler(createMovieSchema), async function(req, res, next){
+  router.post('/', validationHandler(createMovieSchema), async function (req, res, next) {
     const { body: movie } = req;
     try {
       const createMovieId = await movieServices.createMovie({ movie })
@@ -49,11 +52,11 @@ function moviesApi(app){
     }
   });
 
-  router.put('/:movieId', validationHandler({ movieId: movieIdSchema }, 'params'), validationHandler(updateMovieSchema),  async function(req, res, next){
+  router.put('/:movieId', validationHandler({ movieId: movieIdSchema }, 'params'), validationHandler(updateMovieSchema), async function (req, res, next) {
     const { movieId } = req.params;
     const { body: movie } = req;
     try {
-      const updatedMovieId = await movieServices .updateMovie({ movieId, movie })
+      const updatedMovieId = await movieServices.updateMovie({ movieId, movie })
       res.status(200).json({
         data: updatedMovieId,
         message: 'Movie updated!'
@@ -63,7 +66,7 @@ function moviesApi(app){
     }
   });
 
-  router.delete('/:movieId', validationHandler({ movieId: movieIdSchema }, 'params') ,async function(req, res, next){
+  router.delete('/:movieId', validationHandler({ movieId: movieIdSchema }, 'params'), async function (req, res, next) {
     const { movieId } = req.params;
     try {
       const deletedMovieId = await movieServices.deleteMovie({ movieId })
@@ -76,21 +79,21 @@ function moviesApi(app){
     }
   });
 
-  router.patch("/:movieId", async function(req,res,next) {
-		const {movieId} = req.params;
-		const {body: movie} = req;		
-		try {
-			const updatedMovieId = await movieServices.partialUpdateMovie({movieId,movie});
+  router.patch("/:movieId", async function (req, res, next) {
+    const { movieId } = req.params;
+    const { body: movie } = req;
+    try {
+      const updatedMovieId = await movieServices.partialUpdateMovie({ movieId, movie });
 
-			res.status(200).json({
-				data:updatedMovieId,
-				message: "movie updated partially"
-			});
-		}
-		catch(error) {
-			next(error);
-		}
-	});
+      res.status(200).json({
+        data: updatedMovieId,
+        message: "movie updated partially"
+      });
+    }
+    catch (error) {
+      next(error);
+    }
+  });
 }
 
 module.exports = moviesApi;
